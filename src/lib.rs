ad931ec71191380bfaf2be834276c0e27296dc1b
ChousX <gmanenergy@gmail.com>
2024-10-11 21:07:18 -0400

    cleaned it up a bit

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 pub use bevy::prelude::*;
-use bevy::{scene::ron::de, sprite::Mesh2dHandle, utils::HashMap};
+use bevy::{
+    render::{
+        mesh::Indices, render_asset::RenderAssetUsages, render_resource::PrimitiveTopology
+    }, sprite::Mesh2dHandle, utils::HashMap
+};
 
 mod gpu;
 mod cpu;
@@ -11,11 +15,11 @@ impl Plugin for BirdBoxesPlugin{
             .init_resource::<ChunkSize>()
             .init_resource::<ChunkIsoReselution>()
             .add_systems(PreUpdate, add_mesh)
-            .add_systems(Update, update_mesh)
+            .add_systems(Update, update_mesh);
     }
 }
 
-#[derive(Resource, Debug, DeRef)]
+#[derive(Resource, Debug)]
 pub struct ChunkSize(usize, usize);
 impl ChunkSize {
     pub fn x(&self) -> usize { self.0 }
@@ -31,13 +35,7 @@ impl Default for ChunkSize{
     }
 }
 
-impl From<&ChunkSize> for (usize, usize){
-    fn from(self) -> Self{
-        (self.x(), self.y())
-    }
-}
-
-#[derive(Resource, Debug, DeRef)]
+#[derive(Resource, Debug, Deref)]
 pub struct ChunkIsoReselution(pub f32);
 
 impl ChunkIsoReselution{
@@ -49,7 +47,7 @@ impl Default for ChunkIsoReselution{
     }
 }
 
-#[derive(Component)]
+#[derive(Component, Deref, DerefMut, Default)]
 pub struct IsoField(Vec<f32>);
 impl IsoField {
     pub fn new(chunk_size: impl Into<(usize, usize)>) -> Self{
@@ -59,25 +57,25 @@ impl IsoField {
     }
     fn index(pos: impl Into<(usize, usize)>, size: impl Into<(usize, usize)>) -> usize {
         let (x, y): (usize, usize) = pos.into();
-        let (_, sy): (usize, usize) =  size.into();
+        let (sx, _): (usize, usize) =  size.into();
         sx * y + x
     }
     pub fn get(&self, pos: impl Into<(usize, usize)>, size: impl Into<(usize, usize)>) -> f32{
         let i = Self::index(pos, size);
         self[i]
     }
-    pub fn set(&mut self, pos: impl Into<(usize, usize)>, element: f32){
+    pub fn set(&mut self, size: impl Into<(usize, usize)>, pos: impl Into<(usize, usize)>, element: f32){
         let i = Self::index(pos, size);
         self[i] = element;
     }
 
     pub fn build_mesh(&self, size: (usize, usize), resolution: f32 ) -> Mesh{
-        let mut used_indices = HashMap<usize, usize>::new();
+        let mut used_indices = HashMap::<usize, usize>::new();
         let mut vertexes = Vec::new();
-        let mut indices = Vec<usize>::new();
-        let mut normals = Vec<Vec3>::new();
-        let mut face_count = Vec<usize>::new();
-        let mut uvs = Vec<Vec2>::new();
+        let mut indices = Vec::<u32>::new();
+        let mut normals = Vec::<Vec3>::new();
+        let mut face_count = Vec::<usize>::new();
+        let mut uvs = Vec::<Vec2>::new();
         for x in 0..(size.0 - 1){
             for y in 0..(size.1 - 1){
 
@@ -88,22 +86,22 @@ impl IsoField {
                     self.get((x + 1, y), size), // bottom right
                 ]; 
                 let case = get_case(sample);
-                for tri_lists in CASE_TABLE[case].iter(){
-                    for tri in tri_list.iter().filter_map(|&tri| {
-                        if tri[0] == -1{
+                for tri_list in CASE_TABLE[case].iter(){
+                    for index in tri_list.iter().filter_map(|&index| {
+                        if index == -1{
                             None
                         } else {
-                            Some(tri)
+                            Some(index as usize)
                         }
                     }){
-                        for indice in reletive_indices_to_literal_indices((x, y), size, tri).iter() {
+                        for indice in reletive_indices_to_literal_indice((x, y), size, index).iter() {
                             if let Some(corrected_indice) = used_indices.get(indice){
-                                tri_list_out.push(corrected_indice);
+                                indices.push(*corrected_indice as u32);
                             } else {
                                 let index = indices.len();
-                                used_indices.insert(indice, index);
-                                vertexes.push(new_vertex(size, indice));
-                                indices.push(index);
+                                used_indices.insert(*indice, index);
+                                vertexes.push(new_vertex(size, *indice));
+                                indices.push(index as u32);
                                 normals.push(Vec3::new(0.0, 0.0, 0.0));
                                 uvs.push(Vec2::new(0.0, 0.0));
                                 face_count.push(0);
@@ -115,34 +113,39 @@ impl IsoField {
         }
 
         for tri_start in (0..(indices.len())).step_by(3){
-            let tri_noraml = vertexes[tri_start].normal() + vertexes[tri_start +1].normal() + vertexes[tri_start + 2].normal();
+            let tri_noraml = vertexes[tri_start].normalize() + vertexes[tri_start +1].normalize() + vertexes[tri_start + 2].normalize();
             face_count[tri_start]     += 1;
             face_count[tri_start + 1] += 1;
             face_count[tri_start + 2] += 1;
         }
 
         for i in 0..vertexes.len(){
-            normals[i] = (normals[i] / face_count[i]).normal();
+            normals[i] = (normals[i] / face_count[i] as f32).normalize();
         }
 
         Mesh::new(
             PrimitiveTopology::TriangleList,
             RenderAssetUsages::default(),
         )
-            .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices)
+            .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertexes)
             .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, normals)
             .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, uvs)
-            .with_inserted_indices(indices)
+            .with_inserted_indices(Indices::U32(indices))
     }
 }
 
-fn reletive_indices_to_literal_indices(pos: (usize, usize), size: (usize, usize), tri: &[usize]) -> [usize; 3]{
+fn reletive_indices_to_literal_indice(pos: (usize, usize), size: (usize, usize), indice: usize) -> [usize; 3]{
     todo!()
 }
 
 fn new_vertex(size: (usize, usize), index: usize) -> Vec3{
     todo!()
 }
+
+fn get_case(sample: [f32; 4]) -> usize{
+    todo!()
+}
+
 
 #[derive(Bundle, Default)]
 pub struct ChunkBundle {
@@ -175,13 +178,15 @@ fn update_mesh(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    chunk_size: Res<ChunkSize>,
+    chunk_reselution: Res<ChunkIsoReselution>,
 ){
     for (iso_field, pos, Mesh2dHandle(mesh_id)) in chunk_query.iter(){
         let mesh = iso_field.build_mesh(chunk_size.get(), chunk_reselution.get());
-        if let Some(mesh_entity) = meshes.get_mut(mesh_id){
-            mesh_entity = mesh;
+        if let Some(mut mesh_entity) = meshes.get_mut(mesh_id){
+            *mesh_entity = mesh;
         } else {
-            pandic!("Mesh that should be there is not!")
+            panic!("Mesh that should be there is not!")
         }
     }
 }
